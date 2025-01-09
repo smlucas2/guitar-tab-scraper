2309b65076d115a2b75b2cf5aa02626093b6811f
Sean Lucas <smlucas1993@gmail.com>
2025-01-09 14:50:33 -0600

    starting to put all details into a centralised list

--- python/tab_scraper.py
+++ python/tab_scraper.py
@@ -9,6 +9,8 @@ import requests
 from bs4 import BeautifulSoup
 
 class Scraper:
+    all_song_details = []
+    
     def scrape_pages(self, pages):
         for page in range(0, pages):
             self.scrape_for_song_urls(page + 1)
@@ -30,25 +32,31 @@ class Scraper:
             self.scrape_song(cleaned_song_url)
         
     def scrape_song(self, song_url):
-        print(song_url)
+        song_details = [song_url]
+        
         response = requests.get(song_url)
         content = BeautifulSoup(response.content, 'html.parser')
         div_content = content.find('html').find('body').find('div', class_='js-store')
         div_content_str = str(div_content)
         
         tab_sections_raw = div_content_str.split('[/tab]')
+        tab_complete = ''
         for index, tab_section in enumerate(tab_sections_raw):
             #we don't care want the last item in the list, its just html content
             if index == len(tab_sections_raw) - 1:
                 break
             tab_lines = tab_section.split('[tab]')[1]
             tab_lines_cleaned = tab_lines.split('\\r\\n')
+            
+            tab_section_complete = ''
             for tab_line in tab_lines_cleaned:
                 #removes special characters
                 tab_line = tab_line.encode().decode('unicode_escape')
-                print(tab_line)
-            print('\n')
-        print('\n')
-        
+                tab_section_complete = tab_section_complete + tab_line + '\n'
+            tab_section_complete = tab_section_complete[:-1]
+            tab_complete = tab_complete + tab_section_complete + '\n'
+            
+        song_details.append(tab_complete)
+                
 scraper = Scraper()
 scraper.scrape_pages(1)
