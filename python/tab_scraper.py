91bc84f83cf323a8f64d3ea6226f1e4a85d0db35
Sean Lucas <smlucas1993@gmail.com>
2025-01-13 14:38:05 -0600

    refactored code to be more readable

--- python/tab_scraper.py
+++ python/tab_scraper.py
@@ -2,7 +2,7 @@
 """
 Tab Scraper
 
-This utility will scrap the "Ultimate Guitar" website and obtain the tabulations.
+This utility will scrapethe "Ultimate Guitar" website and obtain the tabulations.
 """
 
 import requests
@@ -13,50 +13,71 @@ class Scraper:
     
     def scrape_pages(self, pages):
         for page in range(0, pages):
-            self.scrape_for_song_urls(page + 1)
+            self._scrape_tab_urls(page + 1)
         
-    def scrape_for_song_urls(self, page):
-        if page == 1:
-            response = requests.get('https://www.ultimate-guitar.com/explore?type[]=Tabs')
-        else:
-            response = requests.get('https://www.ultimate-guitar.com/explore?page=' + str(page) + '&type[]=Tabs')
+    def _scrape_tab_urls(self, page):
+        response = self._request_tab_explorer_page(page)
             
+        song_urls = self._parse_tab_urls(response)
+        for song_url in song_urls:
+            self._scrape_song_details(song_url)
+        
+    def _scrape_song_details(self, song_url):
+        song_details = [song_url]
+        
+        song_content = self._parse_song_content(song_url)
+        tab_complete = self._parse_tab_sections(song_content)
+        song_details.append(tab_complete)
+        
+        self.all_song_details.append(song_details)
+        
+    def _request_tab_explorer_page(self, page):
+        if page == 1:
+            return requests.get('https://www.ultimate-guitar.com/explore?type[]=Tabs')
+        else:
+            return requests.get('https://www.ultimate-guitar.com/explore?type[]=Tabs&page=' + str(page))
+        
+    def _parse_tab_urls(self, response):
         content = BeautifulSoup(response.content, 'html.parser')
         div_content = content.find('html').find('body').find('div', class_='js-store')
         div_content_str = str(div_content)
-        
         song_urls = div_content_str.split('tab_url&quot;:&quot;')
+            
+        return self._clean_tab_urls(song_urls)
+    
+    def _clean_tab_urls(self, song_urls):
+        cleaned_song_urls = []
         #first element is the div tag, will skip that
         for song_url in song_urls[1:]:
             cleaned_song_url = song_url.split('&')[0]
-            self.scrape_song(cleaned_song_url)
-        
-    def scrape_song(self, song_url):
-        song_details = [song_url]
-        
+            cleaned_song_urls.append(cleaned_song_url)
+            
+        return cleaned_song_urls
+    
+    def _parse_song_content(self, song_url):
         response = requests.get(song_url)
         content = BeautifulSoup(response.content, 'html.parser')
         div_content = content.find('html').find('body').find('div', class_='js-store')
-        div_content_str = str(div_content)
-        
-        tab_sections_raw = div_content_str.split('[/tab]')
-        tab_complete = ''
-        for index, tab_section in enumerate(tab_sections_raw):
-            #we don't care want the last item in the list, its just html content
-            if index == len(tab_sections_raw) - 1:
-                break
-            tab_lines = tab_section.split('[tab]')[1]
-            tab_lines_cleaned = tab_lines.split('\\r\\n')
+        return str(div_content)
+    
+    def _parse_tab_sections(self, song_content):
+        tab_sections_dirty = song_content.split('[/tab]')
+        tab_complete = []
+        #we don't want the last item in the list, its just html content
+        for index, tab_section_dirty in enumerate(tab_sections_dirty[:-1]):
+            tab_strings = tab_section_dirty.split('[tab]')[1]
+            tab_strings = tab_strings.split('\\r\\n')
             
-            tab_section_complete = ''
-            for tab_line in tab_lines_cleaned:
+            tab_section_cleaned = ''
+            for tab_string in tab_strings:
                 #removes special characters
-                tab_line = tab_line.encode().decode('unicode_escape')
-                tab_section_complete = tab_section_complete + tab_line + '\n'
-            tab_section_complete = tab_section_complete[:-1]
-            tab_complete = tab_complete + tab_section_complete + '\n'
+                tab_string = tab_string.encode().decode('unicode_escape')
+                tab_section_cleaned = tab_section_cleaned + tab_string + '\n'
+            #removes final trailing newline char
+            tab_section_cleaned = tab_section_cleaned[:-1]
+            tab_complete.append(tab_section_cleaned)
             
-        song_details.append(tab_complete)
+        return tab_complete
                 
 scraper = Scraper()
 scraper.scrape_pages(1)
